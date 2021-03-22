47b393642e5280d4c0235fd5c42a40a6edea678f
Sabeel Khan <sabeelhps@gmail.com>
2021-03-22 20:45:38 +0530

    Lecture-12

---
+++ Lecture-12/DOM-1/app.js
@@ -0,0 +1,19 @@
+// const image = document.querySelector('img');
+
+
+
+// let isTrue = false;
+
+// setInterval(() => {
+    
+//     if (isTrue) {
+//         image.setAttribute('src','https://images.unsplash.com/photo-1489493585363-d69421e0edd3?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI5fDZzTVZqVExTa2VRfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+//     }
+//     else {
+//         image.setAttribute('src','https://images.unsplash.com/photo-1613733895930-4a51e90d50fe?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fDZzTVZqVExTa2VRfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60')
+//     }
+
+//     isTrue = !isTrue;
+
+
+// }, 1500);
