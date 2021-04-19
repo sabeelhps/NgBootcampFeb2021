ace546416e7bbaeefbbdf2e20d7c31e17b45a42e
Sabeel Khan <sabeelhps@gmail.com>
2021-04-19 22:03:58 +0530

    Lecture-22

--- Lecture-22/MongooseBasics/index.js
+++ Lecture-22/MongooseBasics/index.js
@@ -10,17 +10,51 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
     });
 
 
+// const moviesSchema = new mongoose.Schema({
+//     name: String,
+//     year: Number,
+//     score: Number,
+//     isWatched: Boolean
+// })
+
 const moviesSchema = new mongoose.Schema({
-    name: String,
-    year: Number,
-    score: Number,
-    isWatched: Boolean
+    name: {
+        type: String,
+        required: true,
+        maxLength:30
+    },
+    year: {
+        type:Number
+    },
+    score: {
+        type: Number,
+        min: 0,
+        max:10
+    },
+    isWatched: {
+        type: Boolean,
+        default:false
+    }
 })
 
 
 const Movie = mongoose.model('Movie', moviesSchema);
 
-// const ironman=new Movie({ name: 'Ironman', year: 2010, score: 7, isWatched: false });
+const ironman=new Movie({year: 2010, score: 7 });
+
+
+ironman.save()
+    .then(m => {
+        console.log(m);
+    })
+    .catch(err => {
+        console.log("OH No error");
+        console.log(err);
+    });
+
+
+
+
 // const superman=new Movie({ name: 'Superman', year: 2015, score: 8, isWatched: true });
 
 
