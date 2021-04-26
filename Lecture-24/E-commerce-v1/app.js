cc6e756a6cdf3656e636851d29c0c56da3ea35e7
Sabeel Khan <sabeelhps@gmail.com>
2021-04-26 22:37:25 +0530

    Lecture-24

--- Lecture-24/E-commerce-v1/app.js
+++ Lecture-24/E-commerce-v1/app.js
@@ -3,13 +3,17 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const seedDB = require('./seed');
+const methodOverride = require('method-override');
 
+
+// Routes
 const productRoutes = require('./routes/product');
 
 mongoose.connect('mongodb://localhost:27017/shopApp',
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify:false
     })
     .then(() => {
         console.log("DB Connected");
@@ -25,18 +29,15 @@ mongoose.connect('mongodb://localhost:27017/shopApp',
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.static(path.join(__dirname, '/public')));
-
-
-app.use(productRoutes);
-
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     
     res.send("LANDING PAGE");
 })
 
-
-
+app.use(productRoutes);
 
 
 app.listen(3000, () => {
--- Lecture-24/E-commerce-v1/routes/product.js
+++ Lecture-24/E-commerce-v1/routes/product.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Product = require('../models/product');
 
 
-
+// Display all the products
 router.get('/products', async(req, res) => {
     
     const products=await Product.find({});
@@ -12,6 +12,53 @@ router.get('/products', async(req, res) => {
 })
 
 
+// Get the form for new product
+router.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
+
+// Create New Product
+router.post('/products', async(req, res) => {
+    
+    console.log(req.body.product);
+
+    await Product.create(req.body.product);
+
+    res.redirect('/products');
+});
+
+
+// Show particular product
+router.get('/products/:id', async(req, res) => {
+    
+    const product=await Product.findById(req.params.id);
+
+    res.render('products/show', { product });
+})
+
+// Get the edit form
+router.get('/products/:id/edit', async(req, res) => {
+
+    const product=await Product.findById(req.params.id);
+
+    res.render('products/edit',{product});
+})
+
+// Upadate the particular product
+router.patch('/products/:id', async(req, res) => {
+    
+    await Product.findByIdAndUpdate(req.params.id, req.body.product);
+
+    res.redirect(`/products/${req.params.id}`)
+})
+
+
+// Delete a particular product
+router.delete('/products/:id', async (req, res) => {
+    await Product.findByIdAndDelete(req.params.id);
+    res.redirect('/products');
+})
 
 
 module.exports = router;
